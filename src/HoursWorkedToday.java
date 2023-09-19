e732dda498912e78749fc0fd95b1db44573fffaa
liamhood1 <lhood24@scienceleadership.org>
2023-09-19 15:04:52 -0400

    j

---
+++ src/HoursWorkedToday.java
@@ -0,0 +1,27 @@
+public class HoursWorkedToday {
+    Employee employee;
+    String startTime;
+    String totalHoursWorked;
+    public static void HoursWorkedToday(String [] args) {
+
+
+
+        void viewShiftHours() {
+            System.out.println(employee + "has worked" + totalHoursWorked);
+        }
+
+        void calculateHourlyWage () {
+            System.out.println(employee + "has earned" + wage);
+
+        }
+
+
+        void updatePunchClock () {
+
+        }
+    }
+
+
+
+
+}
