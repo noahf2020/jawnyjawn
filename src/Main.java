a6d99ab08d2f3b79883b515e62b5a53fd8456ccf
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2023-09-21 09:16:38 -0400

    main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         HoursWorkedToday hoursWorkedToday1 = new HoursWorkedToday();
 
         employee1.name  = "Liam";
-        employee1.wage  = "9.50";
+        employee1.wage  = "9v.50";
         employee1.hoursWorkedToday = hoursWorkedToday1;
         hoursWorkedToday1.totalHoursWorked = "$10";
         hoursWorkedToday1.employee = employee1;
