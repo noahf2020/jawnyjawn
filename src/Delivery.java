95986863b96b9465cf61d8ba3654787373dba872
jadenwilliams <jaw98x@gmail.com>
2023-09-21 08:46:24 -0400

    bv

--- src/Delivery.java
+++ src/Delivery.java
@@ -3,4 +3,18 @@ public class Delivery {
     Destination destination;
     String amountOfPackages;
     Route route;
+
 }
+
+void destination() {
+    System.out.printlin("Today I'm going to " + name);
+}
+
+
+void setAmountOfPackages() {
+    System.out.printlin("I have  ");
+}
+
+void route() {
+    System.out.printlin("Today I'm going to " + name);
+    }
