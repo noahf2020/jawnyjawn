cec68867e8e5f35e497fa14262bd9ba7774893e8
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2023-09-21 08:21:17 -0400

    main

---
+++ src/Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+        int name;
+        int title;
+        int amountOfHours;
+        String  wage;
+
+
+    void checkOutCustomers () {
+        System.out.println("Checking Out Customers");
+    }
+
+    void clockPunchCard () {
+        System.out.println("clocking punch-card");
+
+    }
+    void checkDeliveries (){
+        System.out.println("checking deliveries");
+
+    }
+
+
+
+}
