bc4a17dd846c7fca3163b90e9f377986629b4baf
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2023-09-22 09:42:21 -0400

    main

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,18 @@ public class Main {
 
         Employee employee1 = new Employee();
         HoursWorkedToday hoursWorkedToday1 = new HoursWorkedToday();
+        Delivery delivery1 = new Delivery();
 
         employee1.name  = "Liam";
         employee1.wage  = "9v.50";
-        employee1.hoursWorkedToday = hoursWorkedToday1;
-        hoursWorkedToday1.totalHoursWorked = "$10";
-        hoursWorkedToday1.employee = employee1;
 
+        employee1.hoursWorkedToday = hoursWorkedToday1;
+        hoursWorkedToday1.totalHoursWorked = " $10";
+        hoursWorkedToday1.employee = employee1;
         employee1.clockPunchCard();
 
 
-        Delivery delivery1 = new Delivery();
+
         delivery1.destination = "Philly";
         delivery1.getdestination();
 
